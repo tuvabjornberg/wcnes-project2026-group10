026fa6c6e8b2d54ae7d166b82121bc0e8f18ae8f
Casper <casper.bjorkman@gmail.com>
2026-05-08 10:50:00 +0200

    Stuff and things

--- stats/info_table.py
+++ stats/info_table.py
@@ -15,12 +15,11 @@ NUM_16RND = (
 ) // 2  # how many 16 bits random number included in each frame
 MAX_SEQ = 256  # (decimal) maximum seq number defined by the length of the seq, the length of seq is 1B
 files = {
-    "3,5m-t100cm": "./baseline-exp3-range/baseline3-range-r3,5m-t100cm",
-    "3m-t100cm": "./baseline-exp3-range/baseline3-range-r3m-t100cm",
-    "3m-t200cm": "./baseline-exp3-range/baseline3-range-r3m-t200cm",
-    "4,5m-t100cm": "./baseline-exp3-range/baseline3-range-r4,5m-t100cm",
-    "4,25m-t100cm": "./baseline-exp3-range/baseline3-range-r4,25m-t100cm",
-    "4m-t100cm": "./baseline-exp3-range/baseline3-range-r4m-t100cm",
+    "50cm": "./baseline-exp2/baseline2-50cm.csv",
+    "100cm": "./baseline-exp2/baseline2-100cm.csv",
+    "150cm": "./baseline-exp2/baseline2-150cm.csv",
+    "200cm": "./baseline-exp2/baseline2-200cm.csv",
+    "250cm": "./baseline-exp2/baseline2-250cm.csv",
 }
 
 
@@ -29,18 +28,18 @@ axes = axes.flatten()
 ymax = 0
 
 for ax, (dist, filename) in zip(axes, files.items()):
-    df = readfile(filename + ".csv")
-    df.seq = np.unwrap(df.seq, discont=180, period=256)
+    df = readfile(filename)
+    df.seq = np.unwrap(df.seq, discont=163, period=256)
     df = df[df.payload.apply(lambda x: len(x) == ((PAYLOADSIZE) * 3 - 1))]
     df.reset_index(inplace=True)
 
     bit_errors = (
         compute_ber_packet(row, PACKET_LEN=NUM_16RND * 2) for _, row in df.iterrows()
     )
-
+    ax.set_title(dist)
     ax.plot(df.seq)
     last = df.seq[len(df.seq) - 1]
-    print(f"{dist}: {last} & {200/last:.2f}")
+    print(f"{dist}: {last} & {len(df.seq)/last:.2f}")
 
 # for ax in axes:
 #     ax.set_ylim(0, ymax)j
